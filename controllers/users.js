fc3251ba409ea3330cae7a28261d90a080ae04c7
David Noronha <davidnd97@gmail.com>
2021-11-02 20:57:17 -0300

    CRUD inicial para os usuarios

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,33 @@
 const Users = require('../models/users');
 
 module.exports = app => {
-    app.get('/login', (req, res) => res.send('Hora de Logar!'))
-
-    app.post('/login', (req, res) => {
+    app.post('/user', (req, res) => {
         const user = req.body;
 
-        Users.adiciona(user)
-        res.send('aqui voce esta fazendo login!')
+        Users.adiciona(user, res)
+    })
+
+    app.get('/users', (req, res) => {
+        Users.lista(res);
+    })
+    
+    app.get('/user',(req, res)  =>  {
+        const name = req.query.name;
+
+        Users.buscaPorNome(name, res)
+    })
+
+    app.patch('/user/:user', (req, res) => {
+        const user = req.params.user;
+        const valores = req.body;
+
+        Users.altera(user, valores, res)
+    })
+
+    app.delete('/user/:user', (req, res) => {
+        console.log(req.params.user)
+        const user = req.params.user;
+
+        Users.apaga(user, res)
     })
 }
--- models/users.js
+++ models/users.js
@@ -1,14 +1,63 @@
 const conexao = require('../infraestrutura/conexao');
 
 class Users {
-    adiciona(user) {
+    adiciona(user, res) {
         const sql = 'INSERT INTO Users SET ?';
 
         conexao.query(sql, user, (erro, resultado) => {
             if(erro) {
-                console.log(erro)
+                res.status(400).json(erro)
             } else {
-                console.log(resultado)
+                res.status(201).json(resultado)
+            }
+        })
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Users';
+
+        conexao.query(sql, (erro, resultado) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultado)
+            }
+        })
+    }
+
+    buscaPorNome(name, res) {
+        console.log(name)
+        const sql = `SELECT * FROM Users WHERE name="${name}"`;
+
+        conexao.query(sql, (erro, resultado) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultado)
+            }
+        })
+    }
+
+    altera(name, valores, res) {
+        const sql = 'UPDATE Users SET ? WHERE name=?';
+
+        conexao.query(sql, [valores, name], (erro, resultado) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultado)
+            }
+        })
+    }
+
+    apaga(user, res) {
+        const sql = 'DELETE FROM Users WHERE name=?';
+
+        conexao.query(sql, user, (erro, resultado) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultado)
             }
         })
     }
